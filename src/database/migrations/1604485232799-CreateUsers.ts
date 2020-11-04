c15f4b554938d8b4a120e6108444332d83de1c7a
Macgeiver Knopf <avigaive@hotmail.com>
2020-11-04 17:02:12 -0300

    last commit

--- src/database/migrations/1604485232799-CreateUsers.ts
+++ src/database/migrations/1604485232799-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1604443036327
-  implements MigrationInterface {
+export default class CreateUsers1604485232799 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +14,17 @@ export default class CreateAppointments1604443036327
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -31,13 +35,13 @@ export default class CreateAppointments1604443036327
             name: 'updated_at',
             type: 'timestamp',
             default: 'now()',
-          },          
+          },
         ],
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
